f2c5e602be5e1fda874e52cb2871c5ddeec6ca6d
gyonghua <gyonghua@users.noreply.github.com>
2017-08-08 16:02:18 +0800

    added config for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path")
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require("html-webpack-plugin")
 
-module.exports = {
+var config = {
 entry: "./app/index.js",
 output: {
     path: path.resolve(__dirname, 'dist'),
@@ -23,3 +23,16 @@ plugins: [new HtmlWebpackPlugin({
     template: "app/index.html"
 })]
 }
+
+if (process.env.NODE_ENV === "production") {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            "process.env" : {
+                "NODE_ENV" : JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    )
+}
+
+module.exports = config
